f965985bdf5bb2f9a24934d3af3c9d1b6e414c6f
llcain <llcain2012@gmail.com>
2023-04-27 14:57:51 -0500

    Initialized an array with numbers and curly brackets

--- JavaArrays.java
+++ JavaArrays.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class JavaArrays {
 
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        numbers[0] = 3;
-        numbers[1] = 16;
-        numbers[2] = 10;
+        int[] numbers = {3, 16, 10};
+//        numbers[0] = 3;
+//        numbers[1] = 16;
+//        numbers[2] = 10;
+
+
         System.out.println(Arrays.toString(numbers));
-        System.out.println(numbers.length);
     }
 }
