17ec870679e5c70e5d3c5b784df96215efd148e1
llcain <llcain2012@gmail.com>
2023-04-27 19:16:47 -0500

    Initialized resultArray

--- JavaArrays.java
+++ JavaArrays.java
@@ -1,14 +1,30 @@
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class JavaArrays {
 
     public static void main(String[] args) {
-        int[] numbers = {3, 16, 10};
-//        numbers[0] = 3;
-//        numbers[1] = 16;
-//        numbers[2] = 10;
+       int[] numbers = new int[3];
+        int resultArray[] = new int[6];
 
+        // Array Initialization
+        resultArray[0] = 60;
+        resultArray[1] = 11;
+        resultArray[2] = 80;
+        resultArray[3] = 20;
+        resultArray[4] = 97;
+        resultArray[5] = 45;
+
+
+       numbers[0] = 6;
+       numbers[1] = 7;
+       numbers[2] = 10;
 
         System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(resultArray));
+        System.out.println(resultArray[0]);
+
+
     }
 }
